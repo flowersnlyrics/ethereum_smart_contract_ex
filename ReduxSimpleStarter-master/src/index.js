bc4c56b72ee8801b0873cbd90d3c029847efbc0f
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-27 01:01:24 -0400

    1 video displays with play button

--- ReduxSimpleStarter-master/src/index.js
+++ ReduxSimpleStarter-master/src/index.js
@@ -11,6 +11,7 @@ import YTSearch from 'youtube-api-search';
 // importing our own files we need a file reference 
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail'
 
 const API_KEY = 'AIzaSyDmwRp2qUU042QWsQoPDQ8AeIQ3tYYJ7P8';
 
@@ -42,6 +43,7 @@ class App extends Component  {
       return(
           <div>
               <SearchBar />
+              <VideoDetail video={this.state.videos[0]} />
               <VideoList videos={this.state.videos} /> 
           </div>
       ); 
