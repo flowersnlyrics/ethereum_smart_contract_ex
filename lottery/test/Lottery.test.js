4e7f1bc7f6afb6295e547027c2066ba7146842df
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-26 20:34:10 -0400

    1+ accounts can enter the lottery

--- lottery/test/Lottery.test.js
+++ lottery/test/Lottery.test.js
@@ -26,7 +26,54 @@ describe('Lottery Contract', () => {
         assert.ok(lottery.options.address);
     });
 
+    // What behavior do you really care about with this contract?
 
+    // Any time someone calls enter, make sure their address gets
+    // added if they send the correct amount of money
+    it('allows one account to enter', async() => {
+        await lottery.methods.enter().send({
+            from: accounts[0], 
+            value: web3.utils.toWei('0.02', 'ether') 
+        }); 
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        
+        assert.equal(accounts[0], players[0]); // should be, is 
+        assert.equal(1, players.length); 
+    });
+
+    // able to enter on multiple accounts and that they 
+    // are stored in the players array 
+    it('allows mutiple account to enter', async() => {
+        
+        // 1 account enters
+        await lottery.methods.enter().send({
+            from: accounts[0], 
+            value: web3.utils.toWei('0.02', 'ether') 
+        }); 
+        // 2 accounts enter 
+        await lottery.methods.enter().send({
+            from: accounts[1], 
+            value: web3.utils.toWei('0.02', 'ether') 
+        }); 
+        // 3 accounts enter 
+        await lottery.methods.enter().send({
+            from: accounts[2], 
+            value: web3.utils.toWei('0.02', 'ether') 
+        }); 
+
+
+        const players = await lottery.methods.getPlayers().call({
+            from: accounts[0]
+        });
+        
+        assert.equal(accounts[0], players[0]);  // is, should be 
+        assert.equal(accounts[1], players[1]);  // is, should be 
+        assert.equal(accounts[2], players[2]);  // is, should be 
+        assert.equal(3, players.length); 
+    });
 });
 
 
