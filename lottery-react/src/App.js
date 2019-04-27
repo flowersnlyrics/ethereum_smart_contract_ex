f219bff687265872a431d980e9c954ebed9c6a1e
Jennifer de Souza <jenniferdesouza88@gmail.com>
2019-04-27 17:37:12 -0400

    displaying number of people entered and amount of ether at stake

--- lottery-react/src/App.js
+++ lottery-react/src/App.js
@@ -1,37 +1,52 @@
 import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+//not working with the library but the file that we made
 import web3 from './web3';
-import lottery from './lottery'; //copy of our contract
+
+//get access to our lottery contract on the blockchain
+import lottery from './lottery'; 
 
 class App extends Component{
-    
-    constructor(props) {
-        super(props); 
+    //constructor(props) {
+    //    super(props); 
 
-        this.state = {manager: ''}; 
-    }
-    
+    //    this.state = {manager :''}; 
+    //}
+    //replaced by (means the same thing)
+    state = {
+        manager: '',
+        players: [], 
+        balance: ''
+    }; 
+
     async componentDidMount() {
         const manager = await lottery.methods.manager().call(); 
+        const players = await lottery.methods.getPlayers().call(); 
+        const balance = await web3.eth.getBalance(lottery.options.address); 
 
-        this.setState({manager: manager}); 
+        ///this.setState({manager:manager}); 
+        //we should set our manager with something to start
+        this.setState({manager, players, balance}); //use 2015 syntax 
     }
-
-
-
      render() {
-      
-      //web3.eth.getAccounts() // returns a promise (cant use async/await with a render method of a react component)
-      //   .then(console.log); // chain on the promise with then
 
-        return (
+      console.log(web3.version); //prints out our current verison of web3
+      
+      web3.eth.getAccounts() // returns a promise (cant use async/await with a render method of a react component)
+      .then(console.log); // chain on the promise with then
+
+      return (
           <div>
-            <h2> Lottery Contract </h2>
-            <p> This contract is managed by {this.state.manager}</p>
+            <h2>Lottery Contract</h2>
+            <p> This contract is managed by {this.state.manager}.  
+                There are currently {this.state.players.length} people entered,
+                competing to win {web3.utils.fromWei(this.state.balance,'ether')} ether!
+            </p>
           </div>
-        );
-    }  
+      );
+    }
 }
 
 export default App;
